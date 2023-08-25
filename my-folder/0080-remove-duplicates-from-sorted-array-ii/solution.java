895ef52d13581a75093e80c4c9a5cf8c90d7842a
Jyosna <50022578+jyosna12478@users.noreply.github.com>
2023-08-25 20:15:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.4 MB (99.91%)

---
+++ my-folder/0080-remove-duplicates-from-sorted-array-ii/solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index =2;
+        
+        for (int i=2; i < nums.length; i++)
+        {
+           
+                if (nums[i]!=nums[index-2])
+                {
+                    nums[index]=nums[i];
+                    index=index+1;
+                }
+                
+            
+        }
+        
+        return index;
+    }
+}
+ 
+
