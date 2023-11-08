9b9bfb23d08eac39f7ce0730c11d9f40ed871a7a
Jyosna <50022578+jyosna12478@users.noreply.github.com>
2023-11-08 18:42:58 +0000

    [LeetCode Sync] Runtime - 64 ms (7.45%), Memory - 18.2 MB (51.90%)

---
+++ my-folder/0775-n-ary-tree-preorder-traversal/solution.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    l=[]
+    def preorder(self, root: 'Node') -> List[int]:
+        arr = []
+        def order(root):
+            if root is None: return None
+            arr.append(root.val)
+            for i in root.children:
+                order(i)    
+        order(root)
+        return arr
+
+
+        
