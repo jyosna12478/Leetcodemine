731de954051785974c78c4bad4359a62d975e881
Jyosna <50022578+jyosna12478@users.noreply.github.com>
2023-11-09 11:02:02 +0000

    [LeetCode Sync] Runtime - 47 ms (49.10%), Memory - 18.6 MB (6.31%)

---
+++ my-folder/0776-n-ary-tree-postorder-traversal/solution.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        arr=[]
+        def order(root):
+            if root is None:
+                return None
+            else:
+                for i in root.children:
+                    order(i)
+                arr.append(root.val)
+            
+
+        order(root)
+        return arr
+
